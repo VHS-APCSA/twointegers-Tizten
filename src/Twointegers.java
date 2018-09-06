52da0ed6ad237dbdf2c476fb7bb3073eeca54eb0
bob <bob@APCSA>
2018-09-06 15:34:56 -0700

    Still working on it

---
+++ src/Twointegers.java
@@ -0,0 +1,32 @@
+public class Twointegers
+{
+	private int yin;
+	private int yang;
+	
+	public Twointegers()
+	{
+		yin = 0;
+		yang = 0;
+	}
+	public Twointegers(int yin, int yang)
+	{
+		this.yin = yin;
+		this.yang = yang;
+	}
+	public int getYin()
+	{
+		return yin;
+	}
+	public int getYang()
+	{
+		return yang;
+	}
+	public void setYin(int yin)
+	{
+		this.yin = yin;
+	}
+	public void setYang(int yang)
+	{
+		this.yang = yang;
+	}
+}
